50584278174c1aa6f5a8c713180aadb56af0bf06
Fancychandra <fancychandra@users.noreply.github.com>
2020-09-29 19:54:26 -0700

    Commented and updated discord-bot command

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// This is the code for the discord bot
+// 09-29-2020
+// Created by Dian and Chandra :>
+
+// imports and creates a const of all the discord javascript stuff
 const Discord = require('discord.js');
+// imports and creates a const of secret files
 const config = require('./config.json');
+// makes a new discord client
 const client = new Discord.Client();
 
+// announces that the code is working and ready a singular time
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
+// keeps listening for keyword then sends a message out
 client.on('message', message => {
 	if (message.content === ':>') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send("You're cute :LoveChancake:");
+        // send's back cute nice message
+        message.channel.send("You're cute <a:ALoveChancake:755145813558034633>");
     }
 });
+// discord token
 client.login(config.token);
